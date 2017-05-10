1e8c33dcb1505553caf3ccb68798c8d92fd66864
krendel777 <maslovslava007@gmail.com>
2017-05-10 21:28:11 +0300

    sdf
    sdffds

---
+++ controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+class ProductController
+{
+    public function actionView($id) 
+    {
+        
+        require_once (ROOT . '/views/product/view.php');
+        
+        return true;
+
+
+    }
+}
---
+++ controllers/SiteController.php
@@ -0,0 +1,19 @@
+<?php
+include_once ROOT.'/models/Category.php';
+include_once ROOT.'/models/Product.php';
+
+class SiteController
+{
+    
+    public function actionIndex()
+    {
+        $categories = array();
+        $categories = Category::getCategoriesList();
+       
+        $latestProduct = array();
+        $latestProduct = Product::getLatestProducts(3);
+        require_once (ROOT.'/views/site/index.php');
+        
+        return TRUE;
+    }
+}
